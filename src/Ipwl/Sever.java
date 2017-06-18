717fe6d31b9cb14c2419cb8f9bcfc68806ae351e
jccjd <1340944538@qq.com>
2017-06-18 18:46:18 +0800

    new a IP

---
+++ src/Ipwl/Sever.java
@@ -0,0 +1,35 @@
+package Ipwl;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by lenovo on 2017/6/18.
+ */
+public class Sever {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(9527);
+            System.out.println("server start");
+            Socket socket = server.accept();
+            System.out.println("Creating a customer connection");
+            InputStream input =socket.getInputStream();
+            InputStreamReader isreader = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isreader);
+            while (true){
+                String str = reader.readLine();
+                if (str.equals("exit"))
+                    break;
+                System.out.println("Receive content"+str);
+
+            }
+            System.out.println("disconnect");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
