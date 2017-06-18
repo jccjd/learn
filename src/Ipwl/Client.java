fd401c371add79320ce98f77e34d6becac93bb60
jccjd <1340944538@qq.com>
2017-06-18 18:52:01 +0800

    new a IP

---
+++ src/Ipwl/Client.java
@@ -0,0 +1,24 @@
+package Ipwl;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Created by lenovo on 2017/6/18.
+ */
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 9527);
+            OutputStream out = socket.getOutputStream();
+            out.write("This is my first time visit to server".getBytes());
+            out.write("hello \n".getBytes());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
