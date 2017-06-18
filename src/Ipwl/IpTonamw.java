dbe7c14c722f248f92b0dd3e81ced9007ee64e62
jccjd <1340944538@qq.com>
2017-06-18 18:26:33 +0800

    update

---
+++ src/Ipwl/IpTonamw.java
@@ -0,0 +1,30 @@
+package Ipwl;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by lenovo on 2017/6/18.
+ */
+public class IpTonamw {
+    public static void main(String[] args) {
+        String IP = null;
+        for (int i =100; i<=150; i++){
+            IP = "192.168.1."+i;
+            try {
+                InetAddress host;
+                host = InetAddress.getByName(IP);
+                if(host.isReachable(1000)){
+                    String hostName = host.getHostName();
+                    System.out.println("IP地址" + IP+"的主机名称是"+hostName);
+                }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        System.out.println("搜索完毕");
+    }
+}
