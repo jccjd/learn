9debf76fe2fb3adb2c196a187fd87b2c48ff6610
jccjd <1340944538@qq.com>
2017-06-19 17:12:53 +0800

    new a IP

---
+++ src/Examination_practice/test_1.java
@@ -0,0 +1,16 @@
+package Examination_practice;
+
+/** Print console input
+ * Created by lenovo on 2017/6/19.
+ */
+public class test_1 {
+    public static void main(String[] args) {
+        int i=1;
+        int sum=1;
+        for (int j = 0; j <5; j++) {
+            sum=sum*i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
