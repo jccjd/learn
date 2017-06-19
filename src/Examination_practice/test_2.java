99d6aada2a8ae7fcea5f45e92ad973b2bd2d6700
jccjd <1340944538@qq.com>
2017-06-19 20:41:38 +0800

    new a IP

--- src/Examination_practice/test_2.java
+++ src/Examination_practice/test_2.java
@@ -1,8 +1,24 @@
 package Examination_practice;
 
-/**Print console input
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/** Print console input
  * Created by lenovo on 2017/6/19.
  */
 public class test_2 {
-
+    public static void main(String[] args) {
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb= new StringBuffer();
+        for(int i= 0;i<3;i++){
+            String str=null;
+            try {
+                str=bf.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            sb.append(str);
+        }
+        System.out.println("output string:"+sb.toString());
+    }
 }
