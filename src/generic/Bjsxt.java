927045f69d0f08632881ef18188e039d724840da
jccjd <1340944538@qq.com>
2017-07-06 23:28:08 +0800

    update

---
+++ src/generic/Bjsxt.java
@@ -0,0 +1,16 @@
+package generic;
+
+/**
+ * Created by lenovo on 2017/7/6.
+ */
+public class Bjsxt<T> {
+    public T getStudent() {
+        return student;
+    }
+
+    public void setStudent(T student) {
+        this.student = student;
+    }
+
+    T student;
+}
--- src/generic/StudentApp.java
+++ src/generic/StudentApp.java
@@ -1,6 +1,6 @@
 package generic;
 
-/**泛型嵌套
+/**泛型嵌套 --> 由外到内拆分
  * Created by lenovo on 2017/7/6.
  */
 public class StudentApp {
@@ -8,6 +8,13 @@ public class StudentApp {
         Student<String> stu = new Student<String>();
         stu.setScore("100");
         System.out.println(""+stu.getScore());
+
+        //泛型嵌套
+        Bjsxt<Student<String>> bjsxt = new Bjsxt<>();
+        bjsxt.setStudent(stu);
+        stu = bjsxt.getStudent();
+        String score = stu.getScore();
+        System.out.println(""+score);
     }
 
 }
