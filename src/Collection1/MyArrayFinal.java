a02b83568d7087167ab818fbfebbcae633d21eab
jccjd <1340944538@qq.com>
2017-07-07 21:47:49 +0800

    update

--- src/Collection1/MyArrayFinal.java
+++ src/Collection1/MyArrayFinal.java
@@ -3,20 +3,15 @@ package Collection1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-
 /**
- * 深入理解迭代器的原理 --》一个容器可以创建多个迭代器对象
- * 途径: 使用了内部类|匿名内部类
+ * 使用泛型可以操作多个类型
  *
- * 深入：
- * 1，使用内部类
- * 2.使用Iterable 实现foreach迭代
- * 3.加入末尾添加元素的方法
+ *
  * Created by lenovo on 2017/7/7.
  */
-public class DeepList implements java.lang.Iterable{
+public class MyArrayFinal<E> implements java.lang.Iterable<E>{
     //数组存储
-    private String elem[] = new String[5];
+    private Object elem[] = new Object[5];
     //实际大小
     private int size = 0;
     //计数器 -->指针 游标
@@ -25,7 +20,7 @@ public class DeepList implements java.lang.Iterable{
 //    在末尾添加元素
 
 
-    public void add(String str) {
+    public void add(E str) {
         if (this.size == elem.length) { //容量不足 --> 扩容
             elem = Arrays.copyOf(elem,elem.length*2+1);
         }
@@ -44,9 +39,9 @@ public class DeepList implements java.lang.Iterable{
         }
 
         //    获取下一个
-        public String next() {
+        public E next() {
             coursor++;
-            return elem[coursor];
+            return (E) elem[coursor];
         }
 
         //    删除下一个
@@ -70,7 +65,7 @@ public class DeepList implements java.lang.Iterable{
 
     //使用了内部类实现
     public Iterator iterator2() {
-         class MyIterator implements Iterator {
+        class MyIterator implements Iterator {
             //判断是否有下一个
             public boolean hasNext() {
                 return coursor + 1 < size;
@@ -81,9 +76,9 @@ public class DeepList implements java.lang.Iterable{
             }
 
             //    获取下一个
-            public String next() {
+            public E next() {
                 coursor++;
-                return elem[coursor];
+                return (E) elem[coursor];
             }
 
             //    删除下一个
@@ -102,61 +97,59 @@ public class DeepList implements java.lang.Iterable{
     }
 
     //使用匿名内部类实现
-    public Iterator iterator() {
-        return new Iterator() { //创建Iterator迭代器接口 实现类（匿名）的对象
+    public Iterator<E> iterator() {
+        return new Iterator<E>() { //创建Iterator迭代器接口 实现类（匿名）的对象
 
-                //判断是否有下一个
-                public boolean hasNext() {
-                    return coursor + 1 < size;
-                }
+            //判断是否有下一个
+            public boolean hasNext() {
+                return coursor + 1 < size;
+            }
 
-                public int size() {
-                    return size = elem.length;
-                }
+            public int size() {
+                return size = elem.length;
+            }
 
-                //    获取下一个
-                public String next() {
-                    coursor++;
-                    return elem[coursor];
-                }
+            //    获取下一个
+            public E next() {
+                coursor++;
+                return (E) elem[coursor];
+            }
 
-                //    删除下一个
-                public void remove() {
-                    //移动数组元素
-                    System.arraycopy(elem, coursor + 1, elem, coursor,/*可省略DeepList.this.*/size - (coursor + 1));
-                    //实际大小减一
+            //    删除下一个
+            public void remove() {
+                //移动数组元素
+                System.arraycopy(elem, coursor + 1, elem, coursor,/*可省略DeepList.this.*/size - (coursor + 1));
+                //实际大小减一
            /*可省略DeepList.this.*/
-                    size--;
-                    //游标回退一步
+                size--;
+                //游标回退一步
            /*可省略DeepList.this.*/
-                    coursor--;
-                }
+                coursor--;
+            }
         };
     }
 
     public static void main(String[] args) {
-        DeepList list = new DeepList();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        list.add("c");
-        list.add("c");
-        Iterator it = list.iterator();
-        for (Object object:list) {//foreach增强for循环
-            System.out.println(""+object);
+        MyArrayFinal<Integer> list = new MyArrayFinal<Integer>();
+        list.add(1);// int-->Integer 自动装箱
+        list.add(2);// int-->Integer 自动装箱
+        list.add(3);// int-->Integer 自动装箱
+        for (Integer e:list) {
+            System.out.println(""+e);
         }
-//        while (it.hasNext()) {
-//            System.out.println("" + it.next());
-//        }
-        System.out.println("*********"+list.size);
-
-        ArrayList list1 = new ArrayList();
-        list1.add("A");
-        list1.add("A");
-        list1.add("A");
-        for (Object object:list1) {//foreach增强for循环
-            System.out.println(""+object);
+        System.out.println("————————————————");
+        MyArrayFinal<String> list1 = new MyArrayFinal<String>();
+        list1.add("a");
+        list1.add("b");
+        list1.add("c");
+        for (String a:list1) {
+            System.out.println(""+a);
         }
-
+        System.out.println("————————————————");
+        Iterator<String> it = list1.iterator();
+        while (it.hasNext()) {
+            String e = it.next();
+            System.out.println(""+e);
+        }
     }
 }
