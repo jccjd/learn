118e55b3d8a62788d8601265a211b8e299539ecb
jccjd <1340944538@qq.com>
2017-07-17 10:43:04 +0800

    update

---
+++ src/Ipwl/Tcp/test1/ClientThread.java
@@ -0,0 +1,15 @@
+package Ipwl.Tcp.test1;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by lenovo on 2017/7/17.
+ */
+public class ClientThread {
+    public static void main(String[] args) throws IOException {
+        Socket client = new Socket("localhost",9999);
+        new Thread(new Send(client)).start();//一条路径
+        new Thread(new Receive(client)).start();//一条路径
+    }
+}
