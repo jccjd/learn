7ec0224afe37792f3b9215c90c1f6f141bd2c307
jccjd <1340944538@qq.com>
2017-07-19 19:56:27 +0800

    update

---
+++ src/Ipwl/Server/Server7.java
@@ -0,0 +1,56 @@
+package Ipwl.Server;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * Created by lenovo on 2017/7/18.
+ */
+public class Server7 {
+    private ServerSocket server;
+    private boolean isShutdown = false;
+    public static void main(String[] args) {
+        Server7 server = new Server7();
+        server.start();
+    }
+    public void start() {
+        //默认端口为8888
+        start(8888);
+    }
+    //指定端口
+    public void start(int port) {
+            try {
+                server = new ServerSocket(port);
+                this.receive();
+            } catch (IOException e) {
+                e.printStackTrace();
+                isShutdown = false;
+            }
+    }
+
+    private void receive() {
+        try {
+            while (isShutdown) {
+                new Thread(new Dispatcher(server.accept())).start();
+              }
+            } catch(IOException e){
+                e.printStackTrace();
+                stop();
+        }
+    }
+
+    public void stop() {
+        isShutdown = true;
+        try {
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
