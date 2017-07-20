67e2d282fb2492a07dc881258c30a5e7a6f28d44
jccjd <1340944538@qq.com>
2017-07-20 17:36:20 +0800

    update

---
+++ src/Ipwl/servertest3/LoginServerlet.java
@@ -0,0 +1,27 @@
+package Ipwl.servertest3;
+
+/**
+ * Created by lenovo on 2017/7/19.
+ */
+public class LoginServerlet extends Serverlet {
+
+    @Override
+    public void doGet(Request request, Response response) throws Exception {
+        String name = request.getParameter("uname");
+        String pwd = request.getParameter("pwd");
+        if (login(name, pwd)) {
+            response.println("登录成功");
+        } else {
+            response.println("登录失败");
+
+        }
+    }
+
+    public boolean login(String name, String pwd) {
+        return name.equals("ll") && pwd.equals("123456");
+    }
+    @Override
+    public void doPost(Request request, Response response) throws Exception {
+
+    }
+}
---
+++ src/Ipwl/servertest3/RegisterServerlet.java
@@ -0,0 +1,20 @@
+package Ipwl.servertest3;
+
+/**
+ * Created by lenovo on 2017/7/19.
+ */
+public class RegisterServerlet extends Serverlet {
+    @Override
+    public void doGet(Request request, Response response) throws Exception {
+
+    }
+
+    @Override
+    public void doPost(Request request, Response response) throws Exception {
+        response.println("<html><head><title>test</title></head>");
+        response.println("<body>");
+        response.println("你的用户名为:").println(request.getParameter("uname"));
+        response.println("</body></html>");
+
+    }
+}
