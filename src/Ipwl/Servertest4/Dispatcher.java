d0d7c403a4f0c7ad774291f8298f67c26afb879d
jccjd <1340944538@qq.com>
2017-07-21 14:24:11 +0800

    update

---
+++ src/Ipwl/Servertest4/Dispatcher.java
@@ -0,0 +1,41 @@
+package Ipwl.Servertest4;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Created by lenovo on 2017/7/19.
+ */
+public class Dispatcher implements Runnable {
+    private Socket client;
+    private Request req;
+    private Response rep;
+    private int code = 200;
+
+    @Override
+    public void run() {
+        try {
+            Serverlet serv = WebApp.getServerlet(req.getUrl());
+            if (null == serv) {
+                this.code = 404;
+            } else {
+                serv.service(req, rep);
+            }
+            rep.pushTOCLient(code);
+            } catch (Exception e) {
+                e.printStackTrace();
+                this.code = 500;
+            }
+            rep.pushTOCLient(500);
+    }
+    Dispatcher(Socket client) {
+        this.client = client;
+        try {
+            req = new Request(client.getInputStream());
+            rep = new Response(client.getOutputStream());
+        } catch (IOException e) {
+            code = 500;
+            return;
+        }
+    }
+}
