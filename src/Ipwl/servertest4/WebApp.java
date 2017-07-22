7bffd57765811f73230f54fc28d993088ddb26f7
jccjd <1340944538@qq.com>
2017-07-22 18:42:10 +0800

    update

--- src/Ipwl/servertest4/WebApp.java
+++ src/Ipwl/servertest4/WebApp.java
@@ -7,15 +7,24 @@ import java.util.Map;
  */
 public class WebApp {
     private static ServerletContext context;
+
     static {
+        context = new ServerletContext();
+        Map<String, String> mapping = context.getMapping();
+        mapping.put("/login", "login");
+        mapping.put("/log", "login");
+        mapping.put("/reg", "reglister");
 
+        Map<String, String> serverlet = context.getServerlet();
+        serverlet.put("login", "Ipwl.servertest4.LoginServerlet");
+        serverlet.put("reglister", "Ipwl.servertest4.RegiseterServerlet");
+    }
 
-
-
-    public static Serverlet getServerlet(String url) {
-        if (url == null || (url=url.trim()).equals("")){
+    public static Serverlet getServerlet(String url) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        if (url == null || (url = url.trim()).equals("")) {
             return null;
         }
-        return context.getServerlet().get(context.getMapping().get(url));
+        String name = context.getServerlet().get(context.getMapping().get(url));
+        return (Serverlet)Class.forName(name).newInstance();
     }
 }
