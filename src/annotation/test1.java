369975b01754fb7881da41b9a891d9fa080183ed
jccjd <1340944538@qq.com>
2017-07-25 16:31:07 +0800

    update

--- src/annotation/test1.java
+++ src/annotation/test1.java
@@ -1,9 +1,33 @@
 package annotation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 注解
+ */
+
+@SuppressWarnings("all")
 public class test1 {
 
     @Override
     public String toString() {
         return "";
+    }
+
+    //不建议使用
+    @Deprecated
+    public static void test001() {
+        System.out.printf("test");
+    }
+    //
+
+    public void test002() {
+        List list = new ArrayList();
+
+    }
+
+    public static void main(String[] args) {
+        test001();
     }
 }
