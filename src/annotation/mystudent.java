d8980647f2551a755547a6d6044f11b3e0b773b3
jccjd <1340944538@qq.com>
2017-07-25 17:05:06 +0800

    update

---
+++ src/annotation/mystudent.java
@@ -0,0 +1,34 @@
+package annotation;
+
+
+@myTable("tb_student")
+public class mystudent {
+    private int id;
+     String name;
+     int age;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
