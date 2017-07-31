511ffb9337c6732dd1f412e3874b92b7c1bed163
jccjd <1340944538@qq.com>
2017-07-31 19:48:39 +0800

    update

--- testRhino/src/test1.java
+++ testRhino/src/test1.java
@@ -2,12 +2,18 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 /**
  * 测试脚本引擎执行JavaScript代码
  */
 public class test1 {
-    public static void main(String[] args) throws ScriptException, NoSuchMethodException {
+    public static void main(String[] args) throws ScriptException, NoSuchMethodException, IOException {
         //获取脚本引擎对象
         ScriptEngineManager sem = new ScriptEngineManager();
         ScriptEngine engine = sem.getEngineByName("js");
@@ -29,7 +35,17 @@ public class test1 {
         Object result1 = jsinvoke.invokeFunction("add", new Object[]{12,20});
         System.out.println(result1);
         /*导入其它Java包,使用其它包中的类*/
-        String jscode =""
+        String jscode ="importPackge(java.util); var list = Arrays.asList([\"北京大学\",\"清华大学\"]);";
+        engine.eval(jscode);
+        List<String> list2 = (List<String>) engine.get("list");
+        for (String temp : list2) {
+            System.out.println("temp = [" + temp + "]");
+        }
+        //执行一个js文件
+        URL url = test1.class.getClassLoader().getResource("a.js");
+        FileReader fileReader = new FileReader(url.getPath());
+        engine.eval(fileReader);
+        fileReader.close();
 
     }
 }
