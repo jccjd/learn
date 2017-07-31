1d0efea09a9229c181407a0fbaf1026f582c14d5
jccjd <1340944538@qq.com>
2017-07-31 20:55:47 +0800

    update

---
+++ testRhino/src/a.js
@@ -0,0 +1,5 @@
+function test() {
+    var a = 3;
+    var b = 4;
+    print("invoke js file" + (a + b) );
+}
--- testRhino/src/test1.java
+++ testRhino/src/test1.java
@@ -1,3 +1,4 @@
+
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -19,7 +20,7 @@ public class test1 {
         ScriptEngine engine = sem.getEngineByName("js");
 
         //定义变量,存储到引擎上下文中
-        engine.put("msg","I'm is a good man");
+        engine.put("msg", "I'm is a good man");
         String str = "var user = { name:'lilong', age:20, schools:[ '清华大学','北京大学' ] };";
         str += "print(user.age);";
 
@@ -27,21 +28,24 @@ public class test1 {
         engine.eval(str);
         engine.eval("msg = 'Im not a good man';");
         System.out.println(engine.get("msg"));
+
         //定义函数
         engine.eval("function add(a,b){var sum = a + b; return sum;}");
         /*取得调用接口*/
         Invocable jsinvoke = (Invocable) engine;
+
         /*执行脚本中定义的方法*/
-        Object result1 = jsinvoke.invokeFunction("add", new Object[]{12,20});
+        Object result1 = jsinvoke.invokeFunction("add", new Object[]{12, 20});
         System.out.println(result1);
+
         /*导入其它Java包,使用其它包中的类*/
-        String jscode ="importPackge(java.util); var list = Arrays.asList([\"北京大学\",\"清华大学\"]);";
+        String jscode = "import(java.util); var list = Arrays.asList([\"北京大学\",\"清华大学\"]);";
         engine.eval(jscode);
         List<String> list2 = (List<String>) engine.get("list");
         for (String temp : list2) {
             System.out.println("temp = [" + temp + "]");
         }
-        //执行一个js文件
+        /*执行一个js文件*/
         URL url = test1.class.getClassLoader().getResource("a.js");
         FileReader fileReader = new FileReader(url.getPath());
         engine.eval(fileReader);
