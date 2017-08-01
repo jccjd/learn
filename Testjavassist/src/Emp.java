595107ed13b996e5a95f5183efe0e402d2db57c2
jccjd <1340944538@qq.com>
2017-08-01 17:05:36 +0800

    update

---
+++ Testjavassist/src/Emp.java
@@ -0,0 +1,25 @@
+public class Emp {
+    private int empno;
+    private String empname;
+
+    public Emp(int empno, String empname) {
+        this.empno = empno;
+        this.empname = empname;
+    }
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
+    }
+}
