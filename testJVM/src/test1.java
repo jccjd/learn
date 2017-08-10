7a5f68a0fdb95cf2f6b32c1d2a6913ddb41f3be4
jccjd <1340944538@qq.com>
2017-08-10 21:40:50 +0800

    update

--- testJVM/src/test1.java
+++ testJVM/src/test1.java
@@ -1,13 +1,17 @@
 public class test1 {
+    static {
+        System.out.println("静态初始化test1");
+    }
+
     public static void main(String[] args) {
         new A();
         System.out.println(A.width);
     }
 
 }
-class A {
+class A extends A_Father{
 
-    public static int width = 100;//静态变量/静态域
+    public static int width = 100;//静态变量/静态域 field
     static {
         System.out.println("静态初始化类A");
         width = 300;
@@ -15,5 +19,10 @@ class A {
 
     public A() {
         System.out.println("创建A类的对象");
+    }
+}
+class A_Father {
+    static {
+        System.out.println("静态初始化A_Father");
     }
 }
