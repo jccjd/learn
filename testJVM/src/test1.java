f4a9f9d6dd5c46dafa5dca8aa31d889530000716
jccjd <1340944538@qq.com>
2017-08-10 21:54:03 +0800

    update

--- testJVM/src/test1.java
+++ testJVM/src/test1.java
@@ -3,14 +3,21 @@ public class test1 {
         System.out.println("静态初始化test1");
     }
 
-    public static void main(String[] args) {
-        new A();
-        System.out.println(A.width);
+    public static void main(String[] args) throws ClassNotFoundException {
+//        new A();
+//        System.out.println(A.width);
+//        new A();//只初始化一次
+        Class.forName("test1.A");
+
+
+//        System.out.println(A.width);//类的主动引用(一定会发生类的初始化)
+//        System.out.println(A.Max);//类的被动引用(不会发生类的初始化/在编译阶段已存入常量池中了)
+
     }
 
 }
 class A extends A_Father{
-
+    public static final int Max = 100;
     public static int width = 100;//静态变量/静态域 field
     static {
         System.out.println("静态初始化类A");
