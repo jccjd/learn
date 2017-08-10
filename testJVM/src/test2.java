9a820a4787bd8cb415b932a4cb07f19dd623aa3a
jccjd <1340944538@qq.com>
2017-08-10 22:18:19 +0800

    u

---
+++ testJVM/src/test2.java
@@ -0,0 +1,9 @@
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println(ClassLoader.getSystemClassLoader());
+        System.out.println(ClassLoader.getSystemClassLoader().getParent());
+        System.out.println(ClassLoader.getSystemClassLoader().getParent().getParent());
+        System.out.println(System.getProperty("java.class.path"));
+
+    }
+}
