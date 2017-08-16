a9c8d8e1705b54eba36cf895b7198e13351d9f82
jccjd <1340944538@qq.com>
2017-08-16 18:27:15 +0800

    updata

--- innerClass/src/test1.java
+++ innerClass/src/test1.java
@@ -1,2 +1,31 @@
+
+/**
+ * 测试内部类
+ *
+ * */
 public class test1 {
+
+    /*普通内部类(成员内部类)*/
+    private class FiledInnerClass{
+
+    }
+    void sayHello(){
+
+        /*方法内部类(局部内部类)*/
+        class LocalClass{
+
+        }
+        /*匿名内部类*/
+        Runnable runnable = new Runnable() {//两个作用:1.定义了匿名内部类的类体 2.创建了匿名内部类的对象
+            @Override
+            public void run() {
+
+            }
+        };
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
