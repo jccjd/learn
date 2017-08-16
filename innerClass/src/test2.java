ed8b291c97e4499be4250abec2430ca5e6de77f3
jccjd <1340944538@qq.com>
2017-08-16 18:42:24 +0800

    updata

---
+++ innerClass/src/test2.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * */
+
+public class test2 {
+    int c = 5;
+    static int d = 10;
+
+
+    /*静态内部类*/
+    public static class StaticInnerClass {
+        int a = 3;
+        static int b = 5;
+        public void test() {
+            System.out.println(d);
+        }
+    }
+
+}
