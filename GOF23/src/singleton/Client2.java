978ee19febd6e2a001027daf88952cb65a1d49c5
jccjd <1340944538@qq.com>
2017-08-18 17:51:17 +0800

    updata

---
+++ GOF23/src/singleton/Client2.java
@@ -0,0 +1,12 @@
+package singleton;
+
+/**
+ * 测试反射和反序列化破解单例模式
+ * */
+
+public class Client2 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ GOF23/src/singleton/test4.java
@@ -0,0 +1,8 @@
+package singleton;
+
+/**
+ *
+ * */
+
+public class test4 {
+}
