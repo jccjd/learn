a5b6a28d7989b45f680dd03a391fa767165bbb4f
jccjd <1340944538@qq.com>
2017-08-18 18:14:15 +0800

    updata

--- GOF23/src/singleton/Client2.java
+++ GOF23/src/singleton/Client2.java
@@ -1,12 +1,45 @@
 package singleton;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+
 /**
  * 测试反射和反序列化破解单例模式
  * */
 
 public class Client2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        test4 s1 = test4.getInstance();
+        test4 s2 = test4.getInstance();
+        System.out.println(s1);
+        System.out.println(s2);
+
+
+        /*通过反射的方式直接调用私有构造器*/
+        Class<test4> cls = (Class<test4>) Class.forName("singleton.test4");
+        Constructor<test4> c = cls.getDeclaredConstructor(null);
+        c.setAccessible(true);
+//        test4 s3 = c.newInstance();
+//        test4 s4 = c.newInstance();
+//
+//        System.out.println(s3);
+//        System.out.println(s4);
+
+        /*通过反序列化构造多个对象*/
+        FileOutputStream fos = new FileOutputStream("d:a.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(s1);
+        oos.close();
+        fos.close();
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("d:a.txt"));
+        test4 s3 = (test4) ois.readObject();
+        System.out.println(s3);
+
 
     }
 }
--- GOF23/src/singleton/test4.java
+++ GOF23/src/singleton/test4.java
@@ -1,8 +1,35 @@
 package singleton;
 
+import javafx.beans.binding.ObjectExpression;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
 /**
- *
+ *懒汉式单例模式(如何防止反射和反序列化漏洞)
  * */
 
-public class test4 {
+public class test4 implements Serializable{
+
+    private static test4 instance ;//类初始化时,不加载对象(延时加载,真正用的时候再创建)
+
+    private test4() {
+        /*防反射*/
+        if (instance != null) {
+            throw new RuntimeException();
+        }
+    }
+    /*方法没有同步调用效率高*/
+    public static synchronized test4 getInstance() {
+        if (instance == null) {
+            instance = new test4();
+        }
+        return instance;
+    }
+    /*防反序列化*/
+    private Object readResovle() throws ObjectStreamException {
+        return instance;
+    }
+
 }
