b25ec5b53e74a1b26710754d4aaf57476bc81cfc
jccjd <1340944538@qq.com>
2017-08-23 13:34:27 +0800

    updata

--- GOF23/src/factory/prototype/Client3.java
+++ GOF23/src/factory/prototype/Client3.java
@@ -6,17 +6,17 @@ import java.util.Date;
  *
  * 测试原型模式(浅克隆)
  * */
-public class Client {
+public class Client3 {
     public static void main(String[] args) throws CloneNotSupportedException {
 
         Date date =  new Date(12334444L);
-        Sheep s1 = new Sheep("少理",date);
+        Sheep2 s1 = new Sheep2("少理",date);
+        Sheep2 s2 = (Sheep2) s1.clone();//实现深复制.s2对象的birthday是一个新对象
+
         System.out.println(s1);
         System.out.println(s1.getBirthday());
         System.out.println(s1.getName());
-        date.setTime(222223343L);
-        Sheep s2 = (Sheep) s1.clone();
-        System.out.println(s2.getName());
+        date.setTime(2222222233L);
         System.out.println(s2.getBirthday());
     }
 }
--- GOF23/src/factory/prototype/Sheep2.java
+++ GOF23/src/factory/prototype/Sheep2.java
@@ -2,10 +2,11 @@ package factory.prototype;
 
 
 import java.util.Date;
+
 /**
- * 浅复制
+ * 深复制
  * */
-public class Sheep implements Cloneable{//克隆羊1997
+public class Sheep2 implements Cloneable{//克隆羊1997
 
     private String name;
     private Date birthday;
@@ -14,11 +15,13 @@ public class Sheep implements Cloneable{//克隆羊1997
     protected Object clone() throws CloneNotSupportedException {
         Object object = super.clone();//直接调用Object对象的clone方法
 
-
+        /*添加如下代码实现深克隆(deep clone)*/
+        Sheep2 s = (Sheep2) object;
+        s.birthday = (Date) this.birthday.clone();//把属性也克隆
         return object;
     }
 
-    public Sheep(String name, Date birthday) {
+    public Sheep2(String name, Date birthday) {
         this.name = name;
         this.birthday = birthday;
     }
