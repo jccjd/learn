26ce6df6cb895cf6360d728fc02cebbe38bc65c6
jccjd <1340944538@qq.com>
2017-08-24 12:11:40 +0800

    updata

---
+++ GOF23/src/factory/staticproxy/Client.java
@@ -0,0 +1,12 @@
+package factory.staticproxy;
+
+public class Client {
+    public static void main(String[] args) {
+        Star real = new RealStart();
+        Star proxy = new ProxyStart(real);
+        proxy.confer();
+        proxy.signContract();
+        proxy.bookTicket();
+        proxy.sing();
+    }
+}
