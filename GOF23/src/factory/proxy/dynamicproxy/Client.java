33b4c260a58f0443431d4f8bb61a82ca86607db4
jccjd <1340944538@qq.com>
2017-08-24 14:36:41 +0800

    updata

--- GOF23/src/factory/proxy/dynamicproxy/Client.java
+++ GOF23/src/factory/proxy/dynamicproxy/Client.java
@@ -1,4 +1,13 @@
 package factory.proxy.dynamicproxy;
 
+import java.lang.reflect.Proxy;
+
 public class Client {
+    public static void main(String[] args) {
+        Star realstar = new RealStart();
+        StarHandler handler = new StarHandler(realstar);
+        Star proxy = (Star) Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(),new Class[]{Star.class},handler);
+        proxy.bookTicket();
+        proxy.sing();
+    }
 }
--- GOF23/src/factory/proxy/dynamicproxy/StarHandler.java
+++ GOF23/src/factory/proxy/dynamicproxy/StarHandler.java
@@ -1,22 +1,19 @@
 package factory.proxy.dynamicproxy;
 
-import factory.proxy.staticproxy.Star;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-
 public class StarHandler implements InvocationHandler{
-    private Star realStar;
-
-    public StarHandler(Star realStar) {
-        this.realStar = realStar;
-    }
-
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        method.invoke(realStar,args);
-
+        System.out.println("########");
+        method.invoke(realstar,args);
         return null;
+    }
+
+    Star realstar;
+
+    public StarHandler(Star realstar) {
+        this.realstar = realstar;
     }
 }
