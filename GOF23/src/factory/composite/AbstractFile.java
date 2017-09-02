d2d93c480c7da71691b034b5da9d2f42028878d8
jccjd <1340944538@qq.com>
2017-09-02 18:05:31 +0800

    updata

--- GOF23/src/factory/composite/AbstractFile.java
+++ GOF23/src/factory/composite/AbstractFile.java
@@ -39,8 +39,23 @@ class Folder implements AbstractFile {
     /*定义容器,用来存放本容器下的子结点*/
     private List<AbstractFile> list = new ArrayList<>();
 
+    public Folder(String name) {
+        this.name = name;
+    }
+    public void add(AbstractFile file) {
+        list.add(file);
+    }
+    public void remove(AbstractFile file) {
+        list.remove(file);
+    }
+    public AbstractFile getChild(int index) {
+        return list.get(index);
+    }
     @Override
     public void killVirus() {
-
+        System.out.println("文件夹:" + name+"进行查杀");
+        for (AbstractFile file : list) {
+            file.killVirus();
+        }
     }
 }
