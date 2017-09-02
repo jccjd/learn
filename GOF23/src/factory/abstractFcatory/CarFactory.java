c1e2bac12fed3e0aa7e169c873117039022a74c6
jccjd <1340944538@qq.com>
2017-09-02 16:16:18 +0800

    updata

--- GOF23/src/factory/abstractFcatory/CarFactory.java
+++ GOF23/src/factory/abstractFcatory/CarFactory.java
@@ -1,5 +1,10 @@
 package factory.abstractFcatory;
 
+/**好几天没写代码了
+ * 好难受啊
+ * 先上传水一下
+ * */
+
 public interface CarFactory {
     Engine createEngine();
     Tyre createTyre();
--- GOF23/src/factory/composite/Component.java
+++ GOF23/src/factory/composite/Component.java
@@ -1,5 +1,5 @@
 package factory.composite;
-/**FF
+/**
  *
  * */
 
