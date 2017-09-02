ae9f12cd9b967c65694625b5ed9d18650f893176
jccjd <1340944538@qq.com>
2017-09-02 17:47:11 +0800

    updata

--- GOF23/src/factory/composite/AbstractFile.java
+++ GOF23/src/factory/composite/AbstractFile.java
@@ -1,5 +1,8 @@
 package factory.composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 抽象构建
  * */
@@ -15,6 +18,29 @@ class ImageFile implements AbstractFile {
 
     @Override
     public void killVirus() {
-        System.out.println("");
+        System.out.println("文本文件"+name+"查杀");
+    }
+}
+class VideoFile implements AbstractFile {
+    private String name;
+
+    public VideoFile(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void killVirus() {
+        System.out.println("视频文件"+name+"查杀");
+    }
+}
+class Folder implements AbstractFile {
+
+    private String name;
+    /*定义容器,用来存放本容器下的子结点*/
+    private List<AbstractFile> list = new ArrayList<>();
+
+    @Override
+    public void killVirus() {
+
     }
 }
